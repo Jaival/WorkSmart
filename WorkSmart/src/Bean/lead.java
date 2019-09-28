942daecf07bfeb47420b62893d73cd66efd61daa
Tagore <tagoreamisha@gmail.com>
2019-09-28 17:04:48 +0530

    Lead Bean Class Created

--- WorkSmart/src/Bean/lead.java
+++ WorkSmart/src/Bean/lead.java
@@ -1,8 +1,44 @@
 package Bean;
+import java.io.Serializable;
 
-/**
- * User: Jaival Saija
- * Date: 9/28/2019 4:37 PM
- */
-public class lead {
+public class lead implements Serializable {
+    private int  lead_id;
+    private String lead_name;
+    private String lead_email;
+    private int project_id;
+
+    public lead() {
+    }
+
+    public int getLead_id() {
+        return lead_id;
+    }
+
+    public void setLead_id(int lead_id) {
+        this.lead_id = lead_id;
+    }
+
+    public String getLead_name() {
+        return lead_name;
+    }
+
+    public void setLead_name(String lead_name) {
+        this.lead_name = lead_name;
+    }
+
+    public String getLead_email() {
+        return lead_email;
+    }
+
+    public void setLead_email(String lead_email) {
+        this.lead_email = lead_email;
+    }
+
+    public int getProject_id() {
+        return project_id;
+    }
+
+    public void setProject_id(int project_id) {
+        this.project_id = project_id;
+    }
 }
