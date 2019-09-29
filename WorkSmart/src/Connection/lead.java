c59b49e87686f80cf0fed10bb97ca513bb31d448
Jaival <saijajaival@gmail.com>
2019-09-29 11:45:34 +0530

    Lead Connection Changed

--- WorkSmart/src/Connection/lead.java
+++ WorkSmart/src/Connection/lead.java
@@ -1,8 +1,30 @@
 package Connection;
 
-/**
- * User: Jaival Saija
- * Date: 9/28/2019 4:39 PM
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class lead {
+    Connection connection;
+
+    void insert() throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3360/worksmart", "root", "root");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        PreparedStatement preparedStatement = connection.prepareStatement("Insert into lead values (?,?,?,?)");
+    }
+
+    void select() throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3360/worksmart", "root", "root");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        PreparedStatement preparedStatement = connection.prepareStatement("Select * from lead");
+    }
 }
