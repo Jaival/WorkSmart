348fcea48884756440ba7a8c3f05dac8e32c6450
Tagore <tagoreamisha@gmail.com>
2019-09-29 11:37:29 +0530

    Login Bean Class Changed

--- WorkSmart/src/Connection/login.java
+++ WorkSmart/src/Connection/login.java
@@ -1,8 +1,31 @@
 package Connection;
 
-/**
- * User: Jaival Saija
- * Date: 9/28/2019 4:38 PM
- */
+import java.sql.*;
+
 public class login {
+    Connection connection;
+
+    void insert(int id, String uname, String passwd, String name, String role, String email, String verify) throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/worksmart", "root", "root");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        PreparedStatement ps = connection.prepareStatement("insert into logininfo values (?,?,?,?,?,?,?) ");
+
+    }
+
+    void select() throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/worksmart", "root", "root");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        PreparedStatement ps = connection.prepareStatement("select * from logininfo ");
+        ps.execute();
+    }
 }
+
+
