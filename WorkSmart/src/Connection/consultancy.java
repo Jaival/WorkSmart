e016b47cb47448ebee229fb682199e9ae3a8a59e
kushalvith <43214905+kushalvith@users.noreply.github.com>
2019-09-29 11:36:04 +0530

    consultancy bean class changed

--- WorkSmart/src/Connection/consultancy.java
+++ WorkSmart/src/Connection/consultancy.java
@@ -1,8 +1,55 @@
 package Connection;
 
-/**
- * User: Jaival Saija
- * Date: 9/28/2019 4:38 PM
- */
+import java.sql.*;
+
 public class consultancy {
+
+    void insert() {
+
+
+        try {
+            String ins = "insert into consultancy values(?,?,?,?,?,?)";
+            Class.forName("com.mysql.jdbcDriver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/worksmart", "root", "root");
+            PreparedStatement preparedStatement = connection.prepareStatement(ins);
+
+            preparedStatement.execute();
+
+            connection.close();
+        } catch (ClassNotFoundException | SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    void select() {
+        try {
+            String sel = "select * from  consultancy";
+            Class.forName("com.mysql.jdbcDriver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/worksmart", "root", "root");
+            PreparedStatement preparedStatement = connection.prepareStatement(sel);
+            preparedStatement.execute();
+
+            connection.close();
+        } catch (ClassNotFoundException | SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
